496426770b2351da7f287a21fe837f123b010053
Dan Conrad <dconrad@groupon.com>
2016-09-29 02:06:13 -0500

    fix: all paths for prod

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
+    <Route path="*" component={App} />
   </Router>
   , document.getElementById('root')
 );
