5b6a33769c1b688e16f336b7473a17f54554fec2
Dan Conrad <dconrad@groupon.com>
2016-09-29 01:43:04 -0500

    chore: remove router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-  </Router>
-  , document.getElementById('root')
+  App, document.getElementById('root')
 );
